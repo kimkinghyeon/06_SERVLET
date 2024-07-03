02e51c11e74027e07795208c7cbba0a6ae178496
kim <modan8199@gmail.com>
2024-07-03 14:34:54 +0900

    commit

--- chap11-filter-lecture-source/src/main/java/com/ohgiraffers/section02/uses/EncodingFilter.java
+++ chap11-filter-lecture-source/src/main/java/com/ohgiraffers/section02/uses/EncodingFilter.java
@@ -1,18 +1,36 @@
 package com.ohgiraffers.section02.uses;
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
-import java.io.IOException;
-import jakarta.servlet.annotation.WebServlet;
 
-@WebServlet("/request-service")
-public class EncodingFilter extends HttpServlet {
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.io.IOException;
+
+@WebFilter("/member/*")
+public class EncodingFilter implements Filter {
+
+    private String encodingType;
+
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void init(FilterConfig filterConfig) throws ServletException {
+        encodingType = filterConfig.getInitParameter("encoding-type");
+        System.out.println(encodingType);
 
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        if ("Post".equals(httpServletRequest.getMethod())) {
+            System.out.println("기존 인코딩 타입 : " + httpServletRequest.getCharacterEncoding());
+            httpServletRequest.setCharacterEncoding("UTF-8");
+            System.out.println("변경된 인코딩 타입 : " + httpServletRequest.getCharacterEncoding());
+        }
+        filterChain.doFilter(httpServletRequest,servletResponse);
+    }
 
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
     }
 }
--- chap11-filter-lecture-source/src/main/java/com/ohgiraffers/section02/uses/RegistMemberServlet.java
+++ chap11-filter-lecture-source/src/main/java/com/ohgiraffers/section02/uses/RegistMemberServlet.java
@@ -1,18 +1,30 @@
 package com.ohgiraffers.section02.uses;
+
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
-import java.io.IOException;
-import jakarta.servlet.annotation.WebServlet;
 
-@WebServlet("/request-service")
+import java.io.IOException;
+
+import jakarta.servlet.annotation.WebServlet;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@WebServlet("/member/regist")
 public class RegistMemberServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
-
-    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String userId = request.getParameter("userId");
+        String password = request.getParameter("password");
+        String name = request.getParameter("name");
+
+        System.out.println("name = " + name);
+        System.out.println("userId = " + userId);
+        System.out.println("password = " + password);
+
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        System.out.println("비밀번호가 pass01 인지 확인 : " + passwordEncoder.matches("pass01", password));
+        System.out.println("비밀번호가 pass02 인지 확인 : " + passwordEncoder.matches("pass02", password));
+
 
     }
 }
